879e4b6c30bd3b3ca2ee02999b680aefb464b952
Kathe-fac <katheryne.flor07@gmail.com>
2026-06-25 10:58:12 -0500

    Add GitHub link to perfilData
    Added GitHub link to perfilData and updated formatting.

--- js/data.js
+++ js/data.js
@@ -2,6 +2,7 @@ export const perfilData = {
     nombre: "Katheryne Flor Aimituma Ccallo",
     titulo: "Estudiante de segundo semestre | Universidad Católica San Pablo",
     linkedin: "https://www.linkedin.com/in/katheryne-flor-aimituma-ccallo-52ba1a395/",
+    github: "https://github.com/Kathe-fac",
     avatar: "img/perfil.webp",
     banner: "img/banner.avif",
     descripcion1: "Soy estudiante en la Universidad Católica San Pablo (UCSP), actualmente en segundo semestre. Me interesa el desarrollo web y las tecnologías aplicadas a la innovación.",
